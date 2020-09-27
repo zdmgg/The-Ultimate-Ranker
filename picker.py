8554bd5e4a189c621fdf6b2d07ae47f29e3dcb10
Zach Glindro <66626397+zdmgg@users.noreply.github.com>
2020-09-27 15:28:45 +0800

    Update picker.py

--- picker.py
+++ picker.py
@@ -9,6 +9,9 @@ def exitfunc(i):
         time.sleep(0.5)
         exit()
 
+def calcscore(team):
+    team
+
 print('\n\nSWISS PICKER WITH BUCHHOLZ')
 time.sleep(1)
 print("Type 'exit' whenever you want to exit the program.")
@@ -101,11 +104,26 @@ while True:
 
                 if re.match('[Yy][Ee][Ss]', yn):
                     continue
+                elif len(teams) == 0:
+                    print('You have inputted zero teams. Please try again.')
+                    time.sleep(1)
+                    print("Otherwise, you may type 'exit' to exit the program.\n")
+                    time.sleep(1)
+                    continue
+                elif len(teams) == 1:
+                    print('Picker done!')
+                    time.sleep(1)
+                    print('Scores:')
+
+                    for team in teams.items():
+                        # HERE GOES THE DEFINITION
+
                 elif re.match('[Nn][Oo]', yn):
                     break
 
             # [W, L]
-            teams[team] = [0, 0]
+            teams[team] = [0, 0, 0]
+
         break
     else:
         print('\nPlease type yes/no.')
@@ -123,7 +141,7 @@ if len(teams) % 2 == 1:
     print('that gets to automatically advance to the second round.')
     time.sleep(1.5)
     team = input('\nEnter team: ')
-    
+
     exitfunc(team)
 
 elif len(teams) % 2 == 0:
